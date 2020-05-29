4322a73419bf3e8da64ba995c543937987cbc5e1
Andre Luiz Da Silva Chiapetta <andreluizchiapetta@gmail.com>
2020-05-29 19:58:39 -0300

    aula01 variaveis e tipos

---
+++ aula01 variaveis e tipos de dados/tiposdedados.js
@@ -0,0 +1,31 @@
+string = `Hello World`
+console.log(string)
+console.log(`${typeof string}\n`)
+
+number = 10
+console.log(number)
+console.log(`${typeof number}\n`)
+
+booleanTrue = true
+console.log(booleanTrue)
+console.log(`${typeof booleanTrue}\n`)
+
+booleanFalse = false
+console.log(booleanFalse)
+console.log(`${typeof booleanFalse}\n`)
+
+nulo = null
+console.log(nulo)
+console.log(`${typeof nulo}\n`)
+
+pessoa = {nome: `André`, altura: 1.70, peso: 90, idade: 32}
+pessoa.altura = 1.80
+console.table(pessoa)
+console.log(pessoa[`nome`])
+console.log(`${typeof pessoa}\n`)
+
+array = [`batata`, `cenoura`, `maça`]
+console.table(array)
+console.log(array[0])
+console.log(`${typeof array}\n`)
+
