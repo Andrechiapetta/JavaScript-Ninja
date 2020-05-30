8ca3e6e6c5e746358834ed8f87bc3a0a2f718a2a
Andre Luiz Da Silva Chiapetta <andreluizchiapetta@gmail.com>
2020-05-30 09:57:46 -0300

    aula02 operadores aritméticos

---
+++ aula02 operadores aritmeticos/oparitmeticos.js
@@ -0,0 +1,17 @@
+x = 1
+y = 2
+
+soma = x + y
+console.log(`Soma entre ${x} e ${y} é ${soma}\n`)
+
+subtracao = x - y
+console.log(`Subtração entre ${x} e ${y} é ${subtracao}\n`)
+
+multiplicacao = x * y
+console.log(`Multiplicação entre ${x} e ${y} é ${multiplicacao}\n`)
+
+divisao = x / y
+console.log(`Divisão entre ${x} e ${y} é ${divisao}\n`)
+
+somaMaisIgual= soma += 10
+console.log(`Soma += 10 é ${somaMaisIgual}`)
