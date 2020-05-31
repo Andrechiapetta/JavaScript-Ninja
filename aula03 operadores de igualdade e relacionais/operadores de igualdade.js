03ace813f390c769d03f0d2cd5f6b188b152fab5
Andre Luiz Da Silva Chiapetta <andreluizchiapetta@gmail.com>
2020-05-31 15:11:11 -0300

    aula03 operadores relacionais

--- aula03 operadores de igualdade e relacionais/operadores de igualdade.js
+++ aula03 operadores de igualdade e relacionais/operadores de igualdade.js
@@ -1,3 +1,7 @@
+console.log(`(==) igual`)
+console.log(`(!=) diferente`)
+console.log(`(===) igual e do mesmo tipo`)
+console.log(`(!==) igual e com tipo diferente`)
 var x = 0
 console.log(`x = 0\n`)
 
@@ -12,7 +16,7 @@ console.log(`1 é diferente de 2? (${1 != 2})\n`)
 console.log(`A variavel x é igual a 0? (${x == 0}\n)`)
 
 var um = `1`
-console.log(`A variavel um é igual e do mesmo tipo do que o numero 1? ${um === 1}\n`)
+console.log(`A variavel um é igual ao numero 1 e a variavel um tem o tipo igual do numero 1? ${um === 1}\n`)
 
-console.log(`A variavel um é diferente e com tipo diferente ao numero 1? ${um !== 1}`)
+console.log(`A variavel um é igual ao numero 1 e a variavel um tem tipo diferente do numero 1? ${um !== 1}`)
 
---
+++ aula03 operadores de igualdade e relacionais/operadores relacionais.js
@@ -0,0 +1,10 @@
+console.log(`(<) menor que`)
+console.log(`(>) maior que\n`)
+
+console.log(`1 é menor que o 2 ${1 < 2}\n`)
+
+console.log(`1 é maior que o 2 ${1 > 2}\n`)
+
+console.log(`1 é menor ou igual que o 1 ${1 <= 1}\n`)
+
+console.log(`1 é maior ou igual a 2 ${1 >= 2}`)
