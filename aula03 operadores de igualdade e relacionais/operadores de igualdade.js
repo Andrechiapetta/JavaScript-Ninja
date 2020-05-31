50e96e02b2f1a59e501001087fe7c3d9d974d3f9
Andre Luiz Da Silva Chiapetta <andreluizchiapetta@gmail.com>
2020-05-31 14:36:27 -0300

    aula03 operadores de igualdade

---
+++ aula03 operadores de igualdade e relacionais/operadores de igualdade.js
@@ -0,0 +1,18 @@
+var x = 0
+console.log(`x = 0\n`)
+
+console.log(`1 é igual a 1? (${1 == 1})\n`)
+
+console.log(`1 é igual a 2? (${1 == 2})\n`)
+
+console.log(`1 é diferente de 1 (${1 != 1})\n`)
+
+console.log(`1 é diferente de 2? (${1 != 2})\n`)
+
+console.log(`A variavel x é igual a 0? (${x == 0}\n)`)
+
+var um = `1`
+console.log(`A variavel um é igual e do mesmo tipo do que o numero 1? ${um === 1}\n`)
+
+console.log(`A variavel um é diferente e com tipo diferente ao numero 1? ${um !== 1}`)
+
