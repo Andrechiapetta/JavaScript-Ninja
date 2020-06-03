88402b09c4119949b9af6b37d44f74a5324d4655
Andre Luiz Da Silva Chiapetta <andreluizchiapetta@gmail.com>
2020-06-02 23:33:22 -0300

    operador logico &&

---
+++ aula07OperadoresLogicos/operadoresLogicos.js
@@ -0,0 +1,15 @@
+var x = 3
+var y = 7
+console.log(`Operador (&&) é a mesma coisa que and ou E`)
+
+var e = x === 3 && y === 7
+console.log(`Quando usa && e todas as verficações forem true então tudo é true`)
+console.log(`var e = x === 3 && y === 7`)
+console.log(`console.log(e)`)
+console.log(`${e}\n`)
+
+e = x === 3 && y === 5
+console.log(`Agora se pelo menos uma verificação for false então considera tudo false`)
+console.log(`e = x === 3 && y === 5`)
+console.log(`console.log(e)`)
+console.log(`${e}\n`)
