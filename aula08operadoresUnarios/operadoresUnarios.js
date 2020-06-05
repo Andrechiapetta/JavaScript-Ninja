efa9f666c22cb87ba295b6cd2d15ec5fb023d549
Andre Luiz Da Silva Chiapetta <andreluizchiapetta@gmail.com>
2020-06-04 23:02:25 -0300

    aula 8 operadores unarios

---
+++ aula08operadoresUnarios/operadoresUnarios.js
@@ -0,0 +1,26 @@
+console.log(`Operador unário (+) se for possivel converte o valor que vem após ele em number`)
+console.log(`+"3" no caso aqui "3" é type string`)
+console.log(`${+"3"} agora o unário (+) converteu para type number \n`)
+
+console.log(`Se a string não for possivel converter em number ele retorna (NaN)`)
+console.log(`+"André" a string não sendo numeros não pode ser convertida`)
+console.log(`${+"André"} o operador (+) retornou NaN\n`)
+
+console.log(`O (+) pode ser usado para concatenar`)
+console.log(`"Fer" + "nando"  (+) aqui ira concatenar os dois valores`)
+console.log(`${`Fer` + `nando`}\n`)
+
+console.log(`(+) se for usado com números é realizado uma soma`)
+console.log(`10 + 10  o (+) aqui ira somar os dois valores`)
+console.log(`${10 + 10}\n`)
+
+console.log(`Mas se for um numere em string "10" e um number 10 ele concatena`)
+console.log(`"10" + 10  (+) aqui ira concatenar os dois valores`)
+console.log(`${'10' + 10}\n`)
+
+console.log(`Operador unário (-) se for possivel converte o valor que vem após ele em number e inverte o valor`)
+x = "3"
+console.log(`x = "3"`)
+console.log(`-x  (-) ira converte para number e inverter o seu valor`)
+console.log(`${-x}\n`)
+
