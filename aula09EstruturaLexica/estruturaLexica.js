77b7e420439471c19e005024eccff9bcda6f4118
Andre Luiz Da Silva Chiapetta <andreluizchiapetta@gmail.com>
2020-06-29 12:05:19 -0300

    Estrutura Léxica

--- aula09EstruturaLexica/estruturaLexica.js
+++ aula09EstruturaLexica/estruturaLexica.js
@@ -1,9 +1,9 @@
 var caseSensitive = "JavaScript é case sensitive diferencia letras minusculas de maiusculas"
-console.log(caseSensitive)
+console.log(`${caseSensitive}\n`)
 
 var comentarios = "comentários de linha (//) e de bloco (/**/)"
-console.log(comentarios)
+console.log(`${comentarios}\n`)
 
-console.log("colocando o // no começo da linha o codigo ignora a linha ficando apenas um comentario")
+console.log("colocando o // no começo da linha o codigo ignora a linha ficando apenas um comentario\n")
 
-console.log("colocando o /**/ no começo do bloco o codigo ignora o bloco ficando apenas um comentario")
+console.log("colocando o /**/ no começo do bloco o codigo ignora o bloco ficando apenas um comentario\n")
