b0c0d8b23e21685af42fdf1fef6bbddf8cf4c230
Andre Luiz Da Silva Chiapetta <andreluizchiapetta@gmail.com>
2020-07-08 18:45:50 -0300

    Aula 10 instruçoes condicionais if

--- aula10InstrucoesCondicionais/condicaoIf.js
+++ aula10InstrucoesCondicionais/condicaoIf.js
@@ -1,1 +1,17 @@
-console.log("Tudo que está na condição IF só é executado se for verdadeiro.")
+console.log("Tudo que está na condição IF só é executado se for verdadeiro.\n")
+
+var x = 3;
+console.log(`x = ${x}`)
+
+var y = 7;
+console.log(`y = ${7}\n`)
+
+console.log(`Se x for igual a 3 então y fica sendo 3.`)
+
+if(x === 3){
+    y = 3;
+}
+console.log(`if(x === 3){
+    y = 3;
+}`)
+console.log(`o valor de y é ${y}\n`)
