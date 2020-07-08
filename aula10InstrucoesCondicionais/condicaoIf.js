760886139611a24d007a1675796dc6b58065a363
Andre Luiz Da Silva Chiapetta <andreluizchiapetta@gmail.com>
2020-07-08 18:56:09 -0300

    Aula 10 instruçoes condicionais if

--- aula10InstrucoesCondicionais/condicaoIf.js
+++ aula10InstrucoesCondicionais/condicaoIf.js
@@ -8,10 +8,24 @@ console.log(`y = ${7}\n`)
 
 console.log(`Se x for igual a 3 então y fica sendo 3.`)
 
-if(x === 3){
+if(x === 3) {
     y = 3;
 }
-console.log(`if(x === 3){
+console.log(`if(x === 3) {
     y = 3;
 }`)
 console.log(`o valor de y é ${y}\n`)
+
+console.log(`Podemos utilizar junto ao if o && dessa maneira as duas condição dentro do if tem que ser verdadeira`)
+console.log(`x tem que ser igual a y e x tem que ser igual a 3`)
+
+if(x === y && x === 3) {
+    x = 5;
+    y = 2;
+}
+
+console.log(`if(x === y && x === 3) {
+    x = 5;
+    y = 2;
+}`)
+console.log(`o valor de x é ${x} e o valor de y é ${y}.\n`)
