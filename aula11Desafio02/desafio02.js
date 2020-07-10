91cf658ffc3b1266fa165266920b89a4047b891a
Andre Luiz Da Silva Chiapetta <andreluizchiapetta@gmail.com>
2020-07-09 22:28:54 -0300

    Desafio 02

--- aula11Desafio02/desafio02.js
+++ aula11Desafio02/desafio02.js
@@ -1,27 +1,29 @@
-// Crie uma função que receba dois argumentos e retorne a soma dos mesmos.
-?
+console.log("Crie uma função que receba dois argumentos e retorne a soma dos mesmos.\n") 
 
+function soma(x, y) {
+    return x + y;
+}
 // Declare uma variável que receba a invocação da função criada acima, passando dois números quaisquer por argumento, e somando `5` ao resultado retornado da função.
-?
+
 
 // Qual o valor atualizado dessa variável?
-?
+
 
 // Declare uma nova variável, sem valor.
-?
+
 
 /*
 Crie uma função que adicione um valor à variável criada acima, e retorne a string:
     O valor da variável agora é VALOR.
 Onde VALOR é o novo valor da variável.
 */
-?
+
 
 // Invoque a função criada acima.
-?
+
 
 // Qual o retorno da função? (Use comentários de bloco).
-?
+
 
 /*
 Crie uma função com as seguintes características:
@@ -30,19 +32,19 @@ Crie uma função com as seguintes características:
     Preencha todos os valores corretamente!
 3. O retorno da função deve ser a multiplicação dos 3 argumentos, somando `2` ao resultado da multiplicação.
 */
-?
+
 
 // Invoque a função criada acima, passando só dois números como argumento.
-?
+
 
 // Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
-?
+
 
 // Agora invoque novamente a função criada acima, mas passando todos os três argumentos necessários.
-?
+
 
 // Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
-?
+
 
 /*
 Crie uma função com as seguintes características:
@@ -53,7 +55,6 @@ Crie uma função com as seguintes características:
 5. Se nenhum argumento for passado, retorne o valor booleano `false`.
 6. E ainda, se nenhuma das condições acima forem atendidas, retorne `null`.
 */
-?
+
 
 // Invoque a função acima utilizando todas as possibilidades (com nenhum argumento, com um, com dois e com três.) Coloque um comentário de linha ao lado da função com o resultado de cada invocação.
-?
